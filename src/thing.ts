3b6ef8d9e027032b4ff76ec993e0d638a562ab5a
andy <andy@onepiecejigsaw.com>
2020-02-21 12:21:47 +0000

    Implemented fast-check and property-based testing

--- src/thing.ts
+++ src/thing.ts
@@ -1,3 +1,6 @@
-const thing = true;
 
-export { thing }
+function makeRoundedPercentage(floatingPointNumber:number) {
+    return floatingPointNumber.toFixed(1) + "%";
+}
+
+export { makeRoundedPercentage }
--- test/a.test.ts
+++ test/a.test.ts
@@ -1,7 +1,33 @@
-import { thing } from '../src/thing';
+import { makeRoundedPercentage } from "../src/thing";
+import * as fc from "fast-check";
 
-describe('test a thing', () => {
-   it('should work', () => {
-       expect(true).toBe(thing)
-   })
+describe("properties", () => {
+    it("should be at most 5 characters long", () => {
+        fc.assert(
+            fc.property(fc.float(0, 100), (n: number) => {
+                const result: string = makeRoundedPercentage(n);
+                expect(result.length).toBeLessThanOrEqual(5);
+            })
+        );
+    });
+
+    it("should start with between 1 and 3 digits, followed by an optional dot, followed by an optional digit, and end with a % character", () => {
+        fc.assert(
+            fc.property(fc.float(0, 100), (n: number) => {
+                const result: string = makeRoundedPercentage(n);
+                expect(result).toMatch(/^[0-9]{1,3}[\.]?[0-9]?%$/);
+            })
+        );
+    });
+
+    it("should return a value within 0.05 of the input ", () => {
+        fc.assert(
+            fc.property(fc.float(0, 100), (n: number) => {
+                const result: string = makeRoundedPercentage(n);
+                const numericComponent: number = Number(result.slice(0,-1));
+                expect(Math.abs(numericComponent - n)).toBeLessThanOrEqual(0.05);
+            })
+        );
+    });
+
 });
